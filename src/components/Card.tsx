06cff64440b89b58843faac57b8a3dde6557959c
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2023-07-29 16:46:31 +0200

    added fixed card size

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,24 @@
 export default function Card({ side }: { side: string }) {
-  return <img className="" src={`images/bg-card-${side}.png`} alt="" />;
+  return (
+    <>
+      {side === "back" && (
+        <div className="absolute right-[min(-2.6rem+15.5vw,1rem)] top-8">
+          <img
+            className="w-[17.8125rem] h-[9.763rem]"
+            src={`images/bg-card-${side}.png`}
+            alt=""
+          />
+        </div>
+      )}
+      {side === "front" && (
+        <div className="absolute left-[min(-2.6rem+15.5vw,1rem)] top-[7.52rem]">
+          <img
+            className="w-[17.8125rem] h-[9.763rem]"
+            src={`images/bg-card-${side}.png`}
+            alt=""
+          />
+        </div>
+      )}
+    </>
+  );
 }
--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -2,16 +2,12 @@ import Card from "./Card";
 
 export default function CardContainer() {
   return (
-    <aside className="flex justify-center h-[min(64vw,20rem)] bg-clr-primary-700">
-      <div className="relative w-[min(100%,45rem)]">
-        <figure className="w-[min(76%,25rem)] absolute right-4 top-8">
-          <Card side="back" />
-          <figcaption></figcaption>
-        </figure>
-        <figure className="w-[min(76%,25rem)] absolute z-10 left-4 top-[min(2rem+23.5vw,9.77rem)]">
-          <Card side="front" />
-          <figcaption></figcaption>
-        </figure>
+    <aside
+      className="flex justify-center h-60 bg-clr-primary-700"
+      aria-hidden="true">
+      <div className="relative w-[min(100%,32rem)] text-clr-bg-main">
+        <Card side="back" />
+        <Card side="front" />
       </div>
     </aside>
   );
