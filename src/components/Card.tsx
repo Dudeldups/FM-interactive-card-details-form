807fa11915104d5ec4d5407cfed48bde21a6caa0
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2023-07-30 16:49:16 +0200

    fixed cards and card-text

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,38 @@
 export default function Card({ side }: { side: string }) {
+  let className = {
+    container: `absolute w-[17.8125rem] h-[9.763rem] bg-[url('images/bg-card-${side}.png')] bg-no-repeat bg-cover pl-[1.2rem] pr-[1.3rem] py-[1.1rem] text-[0.5625rem] tracking-[0.08rem] rounded-lg`,
+  };
+
+  switch (side) {
+    case "back":
+      className.container += ` right-[min(-2.6rem+15.5vw,1rem)] top-8`;
+      break;
+
+    case "front":
+      className.container += ` left-[min(-2.6rem+15.5vw,1rem)] top-[7.52rem]`;
+      break;
+  }
+
   return (
-    <>
-      {side === "back" && (
-        <div className="absolute right-[min(-2.6rem+15.5vw,1rem)] top-8">
+    <div className={className.container}>
+      {side === "back" ? (
+        <p className="absolute right-[2.31rem] top-[4.4rem]">000</p>
+      ) : (
+        <>
           <img
-            className="w-[17.8125rem] h-[9.763rem]"
-            src={`images/bg-card-${side}.png`}
+            className="w-[3.375rem] h-[1.875rem]"
+            src="images/card-logo.svg"
             alt=""
           />
-        </div>
+          <p className="mt-[2.3rem] text-[1.125rem] tracking-[0.1375rem]">
+            0000 0000 0000 0000
+          </p>
+          <div className="flex justify-between mt-[1.06rem]">
+            <p className="uppercase">Jane Appleseed</p>
+            <p>00/00</p>
+          </div>
+        </>
       )}
-      {side === "front" && (
-        <div className="absolute left-[min(-2.6rem+15.5vw,1rem)] top-[7.52rem]">
-          <img
-            className="w-[17.8125rem] h-[9.763rem]"
-            src={`images/bg-card-${side}.png`}
-            alt=""
-          />
-        </div>
-      )}
-    </>
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ export default {
         "clr-primary-300": "#dedddf",
         "clr-error": "#ff5252",
       },
+      backgroundImage: {
+        "card-front": "url('images/bg-card-front.png')",
+        "card-back": "url('images/bg-card-back.png')",
+      },
     },
   },
   plugins: [],
