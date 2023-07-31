aae5478e69401ce3f4da604e80cccb7a9336abe6
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2023-07-31 23:05:43 +0200

    added fieldset on form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,29 +6,38 @@ type FormProps = {
 export default function Form({ handleSubmit, handleChange }: FormProps) {
   return (
     <div className="grid place-content-center mt-[5.7rem] px-3 xs:px-6 text-clr-primary-700 uppercase">
-      <form className="grid bg-blue-100" action="#" onSubmit={handleSubmit}>
+      <form
+        className="grid bg-blue-100"
+        autoComplete="off"
+        action="/"
+        method="GET"
+        onSubmit={handleSubmit}>
         <label htmlFor="name">Cardholder name</label>
         <input type="text" id="name" onChange={handleChange} />
+
         <label htmlFor="cardNum">Card number</label>
         <input type="text" id="cardNum" onChange={handleChange} />
 
-        <label htmlFor="expiration-m">
-          <span aria-hidden="true">Exp. date (MM/YY)</span>
-          <span className="sr-only">Expiration month, two digits</span>
-        </label>
+        <div className="grid grid-cols-2">
+          <fieldset>
+            <legend aria-hidden="true">Exp. date (MM/YY)</legend>
 
-        <label className="sr-only" htmlFor="expiration-y">
-          Expiration year, two digits
-        </label>
+            <label className="sr-only" htmlFor="expiration-m">
+              Expiration month, two digits
+            </label>
+            <input type="text" id="expiration-m" onChange={handleChange} />
 
-        <div className="grid grid-cols-[1fr_1fr_2fr] gap-3">
-          <input type="text" id="expiration-m" onChange={handleChange} />
-          <input type="text" id="expiration-y" onChange={handleChange} />
-          <input type="text" id="cvc" onChange={handleChange} />
+            <label className="sr-only" htmlFor="expiration-y">
+              Expiration year, two digits
+            </label>
+            <input type="text" id="expiration-y" onChange={handleChange} />
+          </fieldset>
+
+          <div>
+            <label htmlFor="cvc">CVC</label>
+            <input type="text" id="cvc" onChange={handleChange} />
+          </div>
         </div>
-
-        <label htmlFor="cvc">CVC</label>
-
         <button type="submit">Confirm</button>
       </form>
     </div>
