65ec48dbb9debb540569078c13c9d20ed01ba622
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2023-08-02 22:10:39 +0200

    changed form layout. again.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,9 +5,9 @@ type FormProps = {
 
 export default function Form({ handleSubmit, handleChange }: FormProps) {
   return (
-    <div className="grid place-content-center mt-[5.7rem] px-3 xs:px-6 text-clr-primary-700 uppercase">
+    <div className="grid place-content-center mt-[5.7rem] px-3 xs:px-6 text-xs text-clr-primary-700 uppercase tracking-[0.125rem]">
       <form
-        className="grid bg-blue-100"
+        className="grid max-w-md bg-blue-100"
         autoComplete="off"
         action="/"
         method="GET"
@@ -22,28 +22,30 @@ export default function Form({ handleSubmit, handleChange }: FormProps) {
           <input type="text" id="cardNum" onChange={handleChange} />
         </fieldset>
 
-        <fieldset className="grid">
-          <legend className="" aria-hidden="true">
-            Exp. date (MM/YY)
-          </legend>
+        <div className="grid grid-cols-2 gap-3">
+          <fieldset className="">
+            <legend className="" aria-hidden="true">
+              Exp. date (MM/YY)
+            </legend>
 
-          <div className="grid grid-cols-2">
-            <label className="sr-only" htmlFor="expiration-m">
-              Expiration month, two digits
-            </label>
-            <input type="text" id="expiration-m" onChange={handleChange} />
+            <div className="grid grid-cols-2 gap-3">
+              <label className="sr-only" htmlFor="expiration-m">
+                Expiration month, two digits
+              </label>
+              <input type="text" id="expiration-m" onChange={handleChange} />
 
-            <label className="sr-only" htmlFor="expiration-y">
-              Expiration year, two digits
-            </label>
-            <input type="text" id="expiration-y" onChange={handleChange} />
-          </div>
-        </fieldset>
+              <label className="sr-only" htmlFor="expiration-y">
+                Expiration year, two digits
+              </label>
+              <input type="text" id="expiration-y" onChange={handleChange} />
+            </div>
+          </fieldset>
 
-        <fieldset className="grid">
-          <label htmlFor="cvc">CVC</label>
-          <input type="text" id="cvc" onChange={handleChange} />
-        </fieldset>
+          <fieldset className="grid">
+            <label htmlFor="cvc">CVC</label>
+            <input type="text" id="cvc" onChange={handleChange} />
+          </fieldset>
+        </div>
         <button type="submit">Confirm</button>
       </form>
     </div>
