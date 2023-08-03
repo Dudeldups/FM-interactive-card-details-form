ca0e6d18bbe8d627122fc310d1f731fb6bf5dc61
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2023-08-03 21:51:15 +0200

    added Input component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,3 +1,5 @@
+import Input from "./Input";
+
 type FormProps = {
   handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -5,7 +7,7 @@ type FormProps = {
 
 export default function Form({ handleSubmit, handleChange }: FormProps) {
   return (
-    <div className="grid place-content-center mt-[5.7rem] px-3 xs:px-6 text-xs text-clr-primary-700 uppercase tracking-[0.125rem]">
+    <div className="grid place-content-center mt-16 px-3 xs:px-6 py-6 text-xs text-clr-primary-700 uppercase tracking-[0.125rem]">
       <form
         className="grid max-w-[23.8125rem]"
         autoComplete="off"
@@ -13,22 +15,17 @@ export default function Form({ handleSubmit, handleChange }: FormProps) {
         method="GET"
         onSubmit={handleSubmit}>
         <label htmlFor="name">Cardholder name</label>
-        <input
-          className="mt-2 mb-6 px-4 py-2 text-lg rounded-lg border"
-          type="text"
+        <Input
           id="name"
           placeholder="e.g. Jane Appleseed"
-          onChange={handleChange}
+          handleChange={handleChange}
         />
 
         <label htmlFor="cardNum">Card number</label>
-        <input
-          className="mt-2 mb-6 px-4 py-2 text-lg rounded-lg border"
-          autoComplete="off"
-          type="text"
+        <Input
           id="cardNum"
           placeholder="e.g. 1234 5678 9123 0000"
-          onChange={handleChange}
+          handleChange={handleChange}
         />
 
         <div className="grid grid-cols-2 gap-3 sm:gap-5">
@@ -41,35 +38,29 @@ export default function Form({ handleSubmit, handleChange }: FormProps) {
               <label className="sr-only" htmlFor="expiration-m">
                 Expiration month, two digits
               </label>
-              <input
-                className="mt-2 mb-6 px-4 py-2 text-lg rounded-lg border"
-                type="text"
+              <Input
                 id="expiration-m"
                 placeholder="MM"
-                onChange={handleChange}
+                handleChange={handleChange}
               />
 
               <label className="sr-only" htmlFor="expiration-y">
                 Expiration year, two digits
               </label>
-              <input
-                className="mt-2 mb-6 px-4 py-2 text-lg rounded-lg border"
-                type="text"
+              <Input
                 id="expiration-y"
                 placeholder="YY"
-                onChange={handleChange}
+                handleChange={handleChange}
               />
             </div>
           </fieldset>
 
           <div>
             <label htmlFor="cvc">CVC</label>
-            <input
-              className="w-full mt-2 mb-6 px-4 py-2 text-lg rounded-lg border"
-              type="text"
+            <Input
               id="cvc"
               placeholder="e.g. 123"
-              onChange={handleChange}
+              handleChange={handleChange}
             />
           </div>
         </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,24 @@
-type InputProps = {};
-export default function Input({}: InputProps) {
-  return <div>Input</div>;
+type InputProps = {
+  id: string;
+  placeholder: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function Input({ id, placeholder, handleChange }: InputProps) {
+  let className = "mt-2 mb-6 px-4 py-2 text-lg rounded-lg border";
+
+  if (id === "cvc") {
+    className += " w-full";
+  }
+
+  return (
+    <input
+      key={id}
+      className={className}
+      type="text"
+      id={id}
+      placeholder={placeholder}
+      onChange={handleChange}
+    />
+  );
 }
